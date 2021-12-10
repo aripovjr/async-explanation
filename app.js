c0faf1321a795692090999e682b0e13692f76abf
Jasurbek Aripov <68148826+aripovjr@users.noreply.github.com>
2021-12-10 09:36:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 //PROMISES - Pending, Resolved, Rejected
 // then catch - pass another callback
 
+/*
 const promise = new Promise((resolve, reject)=>{
 let value = true 
 if(value){
@@ -13,6 +14,7 @@ if(value){
 
 promise.then(word=>console.log(word))
 promise.catch(error=>console.log(error))
+*/
 
 //promise object - represents the eventual compelation or failure of asynchronous operation and its resulting
 //promise has three state: 
@@ -24,3 +26,61 @@ rejected - so it means operation failed
 //catch - to rejected results
 
 //try to change the condition to : if (!value) etc
+
+// const heading1 = document.querySelector('.one')
+// const heading2 = document.querySelector('.two')
+// const heading3 = document.querySelector('.three')
+// const btn = document.querySelector('.btn')
+
+// btn.addEventListener('click', ()=>{
+//     addColor(10000, heading1, 'red')
+//     .then(()=>addColor(17000, heading2, 'green')
+//     .then(()=>addColor(5000, heading3, 'yellow')))
+// })
+
+// function addColor(time, element, color){
+//     return new Promise((resolve, reject)=>{
+//         if(element){
+//             setTimeout(()=>{
+//                 element.style.color = color
+//                 resolve()
+//             }, time)
+//         }else{
+//             reject(new Error(`There is no such ${element} element`))       
+//         }
+//     })
+// }
+
+
+const heading1 = document.querySelector('.one')
+const heading2 = document.querySelector('.two')
+const heading3 = document.querySelector('.three')
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', async ()=>{
+    displayColor()
+})
+
+
+async function displayColor(){
+try{
+    await addColor(2000, heading1, 'red')
+    await addColor(2000, heading2, 'green')
+    await addColor(2000, heading3, 'yellow')    
+}catch(error){
+    console.log(error)
+}
+}
+
+function addColor(time, element, color){
+    return new Promise((resolve, reject)=>{
+        if(element){
+            setTimeout(()=>{
+                element.style.color = color
+                resolve()
+            }, time)
+        }else{
+            reject(new Error(`There is no such ${element} element`))       
+        }
+    })
+}
